16d201c46f52afdbd6a959d9f4961758c79cd670
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-18 12:47:12 -0300

    add parameters to functions

--- src/Employee/domain/employeeRepository.ts
+++ src/Employee/domain/employeeRepository.ts
@@ -1,10 +1,13 @@
 import { employeeCreate } from "./dto/employeeCreate";
+import { employeeOrderInput } from "./dto/employeeOrderInput";
 import { employeeUpdate } from "./dto/employeeUpdate";
 import { employeeEntity } from "./employeeEntity";
 
+
 export interface employeeRepository{
-    getEmployees(): Promise<employeeEntity[]| null>;
-    getEmployeeById(id:number) : Promise<employeeEntity | null>;
+    getEmployees(order?:employeeOrderInput): Promise<employeeEntity[]| null>;
+    getEmployeeById(id:string) : Promise<employeeEntity | null>;
     createEmployee(employee:employeeCreate):Promise<employeeEntity|null>;
-    editEmployee(employee:employeeUpdate):Promise<employeeEntity|null>;
+    editEmployee(id:string,employee:employeeUpdate):Promise<employeeEntity|null>;
+    deleteEmployee(id:string):Promise<employeeEntity | null>
 }
