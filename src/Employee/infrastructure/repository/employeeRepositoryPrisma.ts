66bc1286979d0ce60a8402b5bab80a4e04eced4d
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-18 12:47:46 -0300

    create logic to employess methods

--- src/Employee/infrastructure/repository/employeeRepositoryPrisma.ts
+++ src/Employee/infrastructure/repository/employeeRepositoryPrisma.ts
@@ -1,25 +1,55 @@
-//import { prisma } from "../../../shared/infrastructure/data/prisma";
+import { Prisma } from "@prisma/client";
+import { prisma } from "../../../shared/infrastructure/data/prisma";
 import { employeeCreate } from "../../domain/dto/employeeCreate";
+import { employeeOrderInput } from "../../domain/dto/employeeOrderInput";
 import { employeeUpdate } from "../../domain/dto/employeeUpdate";
-
 import { employeeEntity } from "../../domain/employeeEntity";
 import { employeeRepository } from "../../domain/employeeRepository";
 
-export class employeeRepositoryPrisma implements employeeRepository{
 
+export class employeeRepositoryPrisma implements employeeRepository{
     
     
-    async getEmployees(): Promise<employeeEntity[] | null> {
-        throw new Error("Method not implemented.");
+    async getEmployees(order?: employeeOrderInput): Promise<employeeEntity[] | null> {
+        let employees: employeeEntity[] = [];
+        let orderBy: Prisma.EmployeeOrderByWithRelationInput | undefined;
+        
+        if (order) {
+            orderBy = {
+                [order.sort]: order.order
+            };
+        }
+
+        employees = await prisma.employee.findMany({
+            orderBy, //si oirderBy es undefined no aplica ordenamiento
+        });
+
+        return employees;
     }
-    async getEmployeeById(id: number): Promise<employeeEntity | null> {
-        throw new Error("Method not implemented.");
+
+    async getEmployeeById(id: string): Promise<employeeEntity | null> {
+        return await prisma.employee.findUnique({
+            where:{
+                id:id
+            }
+        })
     }
     async  createEmployee(employee: employeeCreate): Promise<employeeEntity | null> {
-        throw new Error("Method not implemented.");
+        return await prisma.employee.create(
+            { data:employee }
+        )
     }
-    async editEmployee(employee: employeeUpdate): Promise<employeeEntity | null> {
-        throw new Error("Method not implemented.");
+    async editEmployee(id:string, employee: employeeUpdate): Promise<employeeEntity | null> {
+        return await prisma.employee.update({
+            where:{id:id},
+            data: employee,
+        })
+    }
+
+    async  deleteEmployee(id: string): Promise<employeeEntity | null> {
+        return await prisma.employee.delete({
+        where:{id:id}
+       })
     }
     
 }
