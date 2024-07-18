959cf874d33a04602c2616eb84d7935f095f6134
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-18 12:46:36 -0300

    rename repository and add employees methods

--- src/Employee/application/employeeUseCases.ts
+++ src/Employee/application/employeeUseCases.ts
@@ -1,10 +1,36 @@
 
 import { employeeCreate } from "../domain/dto/employeeCreate";
+import { employeeOrderInput } from "../domain/dto/employeeOrderInput";
+import { employeeUpdate } from "../domain/dto/employeeUpdate";
 import { employeeEntity } from "../domain/employeeEntity";
 import { employeeRepository } from "../domain/employeeRepository";
 
 export class employeeUseCases {
-    constructor(private readonly employeeRepositoy: employeeRepository){}
+    constructor(private readonly employeeRepository: employeeRepository){}
+
+
+
+    public async getEmployees(sort?: "name" | "lastname" | "email" | "role", order?: "asc" | "desc"): Promise<employeeEntity[] | null> {
+        let employees: employeeEntity[] | null = null;
+
+        if (sort && order) {
+            const orderBy: employeeOrderInput = {
+                sort:sort,
+                order: order
+            };
+
+            employees = await this.employeeRepository.getEmployees(orderBy)
+        } else {
+            employees = await this.employeeRepository.getEmployees();
+        }
+
+        return employees ?? []; // Return an empty array if employees is null
+    }
+
+    public async getEmployeeById(id:string){
+        const employee=  await this.employeeRepository.getEmployeeById(id);
+        return employee;
+    }
 
     public async createEmployee(name:string, lastName:string,email:string, role:boolean){
         const employee:employeeCreate={
@@ -13,7 +39,25 @@ export class employeeUseCases {
             email,
             role
         }
-        const employeeCreated = await this.employeeRepositoy.createEmployee(employee)
+        const employeeCreated = await this.employeeRepository.createEmployee(employee)
         return employeeCreated;
+    }
+
+
+    public async editEmployee(id:string,name:string, lastName:string, email:string, role:boolean){
+        const employe: employeeUpdate={
+            name,
+            lastName,
+            email,
+            role
+        }
+
+        const employeeUpdated = await this.employeeRepository.editEmployee(id,employe)
+        return employeeUpdated
+    }
+
+    public async deleteEmployee(id:string){
+        const employeeDeleted = await this.employeeRepository.deleteEmployee(id)
+        return employeeDeleted;
     }
 }
