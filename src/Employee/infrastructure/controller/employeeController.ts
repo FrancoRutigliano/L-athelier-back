1dd90e845863e76d9af7bbfbbcd710a98fdd3441
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-18 12:45:43 -0300

    create employees methods

--- src/Employee/infrastructure/controller/employeeController.ts
+++ src/Employee/infrastructure/controller/employeeController.ts
@@ -1,12 +1,57 @@
 import { Request,Response } from "express";
 import { employeeUseCases } from "../../application/employeeUseCases";
+import { employeeEntity } from "../../domain/employeeEntity";
 
 export class employeeController{
     constructor(private employeeUseCase: employeeUseCases){}
 
-    public createUser= async(req:Request, res:Response)=>{
+    public getEmployees=async(req:Request,res:Response)=>{
+        const {sort,order}=req.query;
+
+        let employees:employeeEntity[]=[];
+        
+        if(sort&&order&& this.isValidSort(sort)&& this.isValidOrder(order)){
+            employees= await this.employeeUseCase.getEmployees(sort,order)??[];
+        }else{
+            employees = await this.employeeUseCase.getEmployees()??[];
+        }
+
+        return employees;
+    }
+
+    public getEmployeeById=async (req:Request, res:Response)=>{
+        const {id} = req.params;
+        const employee= await this.employeeUseCase.getEmployeeById(id)
+        return employee;
+    }
+
+    public createEmployee= async(req:Request, res:Response)=>{
         const {name, lastName,email, role}= req.body;
         const employeeCreated = await this.employeeUseCase.createEmployee(name,lastName,email,role);
-        console.log(employeeController);
+        return employeeCreated;
+    }
+
+    public editEmployee=async (req:Request,res:Response)=>{
+        const {id}= req.params;
+        const {name, lastName,email, role}= req.body;
+        const employeEdited = await this.employeeUseCase.editEmployee(id,name, lastName,email,role)
+        return employeEdited;
+    }
+
+    public deleteEmployee = async(req:Request,res:Response)=>{
+        const {id}= req.params;
+        const employeeDeleted = await this.employeeUseCase.deleteEmployee(id);
+        return employeeDeleted;
+    }
+
+
+
+
+    private isValidSort(sort: any): sort is "name" | "lastname" | "email" | "role" {
+        return ["name", "lastname", "email", "role"].includes(sort);
+    }
+
+    private isValidOrder(order: any): order is "asc" | "desc" {
+        return ["asc", "desc"].includes(order);
     }
 }
