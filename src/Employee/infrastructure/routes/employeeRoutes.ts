6d1f3e0c94ddec70a467656eb253064c515424ee
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-18 12:47:57 -0300

    create router to employess

--- src/Employee/infrastructure/routes/employeeRoutes.ts
+++ src/Employee/infrastructure/routes/employeeRoutes.ts
@@ -13,8 +13,28 @@ const EmployeeUseCases = new employeeUseCases(EmployeeRepositoryPrisma);
 const EmployeeController = new employeeController(EmployeeUseCases)
 
 
+EmployeeRouter.get(`${path}`,(req:Request,res:Response) => {
+    EmployeeController.getEmployees(req,res);
+ });
+
+
+ EmployeeRouter.get(`${path}/:id`,(req:Request,res:Response) => {
+    EmployeeController.getEmployeeById(req,res);
+ });
+
+
 EmployeeRouter.post(`${path}/new`,(req:Request,res:Response) => {
-    EmployeeController.createUser(req,res);
+    EmployeeController.createEmployee(req,res);
+ });
+
+ 
+ EmployeeRouter.patch(`${path}/edit/:id`,(req:Request,res:Response) => {
+    EmployeeController.editEmployee(req,res);
+ });
+
+ 
+ EmployeeRouter.delete(`${path}/delete/:id`,(req:Request,res:Response) => {
+    EmployeeController.deleteEmployee(req,res);
  });
 
 
