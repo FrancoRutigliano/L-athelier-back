d4c70252dd680d1994b2f519f25df53b6a416a9a
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-22 14:20:24 -0300

    update app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import EmployeeRouter from "./Employee/infrastructure/routes/employeeRoutes";
+import SessionRouter from "./Session/infrastructure/routes/sessionRoutes";
 
 dotenv.config()
 export const app = express();
@@ -8,3 +9,4 @@ app.use(express.json())
 
 
 app.use(EmployeeRouter);
+app.use(SessionRouter)
