7ab78f715d0aace0f48a21244af6a2f7d54a5f3c
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-23 17:07:17 -0300

    create controller of products

--- src/Product/infrastructure/controller/productController.ts
+++ src/Product/infrastructure/controller/productController.ts
@@ -1,27 +1,61 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import { productUseCase } from "../../application/productUseCases";
 
 export class productController {
-    constructor(private readonly productUseCases: productUseCase){}
+  constructor(private readonly productUseCases: productUseCase) {}
 
-    public getProducts=async(req:Request,res:Response)=>{
+  public getProducts = async (req: Request, res: Response) => {
+    const result = await this.productUseCases.getProducts();
 
+    if (result.isSuccess) {
+      return res
+        .status(result.statusCode)
+        .json({ message: result.value, details: true });
     }
+    return res
+      .status(result.statusCode)
+      .json({ message: result.value, details: false });
+  };
+
+  public getProductById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const result = await this.productUseCases.getProductById(id);
+
+    if (result.isSuccess) {
+      return res.status(result.statusCode).json({ message: result.value, details: true });
+    }
+    return res.status(result.statusCode).json({ message: result.value, details: false });
+  };
+
+  public createProduct = async (req: Request, res: Response) => {
+    const {name}=req.body
+    const result = await this.productUseCases.createProduct(name);
     
-    public getProductById=async(req:Request,res:Response)=>{
-        
+    if(result.isSuccess){
+        return res.status(result.statusCode).json({message:result.value,details:true})
     }
+    return res.status(result.statusCode).json({ message: result.value, details: false });
+  };
 
-    public createProduct=async(req:Request,res:Response)=>{
-        
+  public editProduct = async (req: Request, res: Response) => {
+    const{id}=req.params
+    const {name}= req.body
+    const result = await this.productUseCases.editProduct(id,name);
+
+    if(result.isSuccess){
+        return res.status(result.statusCode).json({message:result.value,details:true})
     }
+    return res.status(result.statusCode).json({ message: result.value, details: false });
+  };
 
-    public editProduct=async(req:Request,res:Response)=>{
-        
+  public deleteProduct = async (req: Request, res: Response) => {
+    const{id}=req.params
+    const result = await this.productUseCases.deleteProduct(id);
+
+    if(result.isSuccess){
+        return res.status(result.statusCode).json({message:result.value,details:true})
     }
-
-    public deleteProduct=async(req:Request,res:Response)=>{
-        
-    }
-
+    return res.status(result.statusCode).json({ message: result.value, details: false });
+  };
 }
