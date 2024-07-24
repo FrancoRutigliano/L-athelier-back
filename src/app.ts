e1d0c32fe1aca21a21fa92c1dfa2b7a8691abb44
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-24 11:53:38 -0300

    change file extension

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import EmployeeRouter from "./Employee/infrastructure/routes/employeeRoutes.js";
-import SessionRouter from "./Session/infrastructure/routes/sessionRoutes.js";
+import EmployeeRouter from "./Employee/infrastructure/routes/employeeRoutes";
+import SessionRouter from "./Session/infrastructure/routes/sessionRoutes";
 import session from "express-session";
-import { sessionEntity } from "./shared/infrastructure/middlewares/auth/entity/sessionEntity.js";
+import { sessionEntity } from "./shared/infrastructure/middlewares/auth/entity/sessionEntity";
 
 
 dotenv.config();
