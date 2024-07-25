9193821eaaf3554f575718e887c15156f8405d7f
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-25 15:17:00 -0300

    update middleware JWT

--- src/shared/infrastructure/middlewares/auth/middlewareJWT.ts
+++ src/shared/infrastructure/middlewares/auth/middlewareJWT.ts
@@ -1,67 +1,24 @@
-import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const verifySession = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers['authorization'];
+  const authHeader = req.headers['authorization'];
 
-<<<<<<< HEAD
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized", details: false });
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
   }
 
-  const authToken = token.replace(/^Bearer /i, '').trim(); 
+  const token = authHeader.replace(/Bearer\s+/i, '').trim(); // Clean token and remove Bearer prefix
   const secret = process.env.SECRET_JWT;
 
   if (!secret) {
-    return res.status(500).json({ message: 'JWT Secret not found', details: false });
+    return res.status(500).json({ error: 'Internal server error: Secret key not defined' });
   }
-
+  
   try {
-    const decoded: any = jwt.verify(authToken, secret);
-    
-    if (decoded) {
-      const { id, name, role } = decoded;
-
-      // Inicializa req.session si no está inicializado
-      if (!req.session) {
-        req.session = {} as any;
-      }
-
-     
-      req.session.user = { id, name, role };
-
-      next(); 
-    } else {
-      return res.status(401).json({ error: 'Invalid token', details: false });
-=======
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized", details: false });
-    }
- 
-    const authToken = token.replace(/bearer/gim, '').trim(); // Limpiar el token Bearer y obtener solo el tokenj
-    const secret = process.env.SECRET_JWT;
-
-    if (!secret) {
-        return res.status(500).json({ error: 'Error interno del servidor' });
-    }
-
-    try {
-        const decoded: any = jwt.verify(authToken, secret); 
-
-        if (decoded && typeof decoded === 'object' && decoded.id && decoded.email && decoded.role) {
-            const { id, email, role } = decoded; 
-
-            //req.session.user = { id, email, role };
-
-            next(); 
-        } else {
-            return res.status(401).json({ error: 'Token inválido' });
-        }
-    } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
->>>>>>> 54869ecb06739731df7562564135c4c6761d92e9
-    }
+    jwt.verify(token, secret);
+    next(); // Token is valid, proceed to the next middleware/route
   } catch (error) {
-    return res.status(500).json({ message: 'Error verifying token', details: false });
+    return res.status(401).json({ error: 'Invalid or expired token' });
   }
 };
