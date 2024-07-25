49d3d9253952a348b919567d257e9799a8f5db4e
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-25 08:37:10 -0300

    resolved error in authentication middleware

--- src/shared/infrastructure/middlewares/auth/middlewareJWT.ts
+++ src/shared/infrastructure/middlewares/auth/middlewareJWT.ts
@@ -1,33 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+
 export const verifySession = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers['authorization'];
+  const token = req.headers['authorization'];
 
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized", details: false });
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized", details: false });
+  }
+
+  const authToken = token.replace(/^Bearer /i, '').trim(); 
+  const secret = process.env.SECRET_JWT;
+
+  if (!secret) {
+    return res.status(500).json({ message: 'JWT Secret not found', details: false });
+  }
+
+  try {
+    const decoded: any = jwt.verify(authToken, secret);
+    
+    if (decoded) {
+      const { id, name, role } = decoded;
+
+      // Inicializa req.session si no está inicializado
+      if (!req.session) {
+        req.session = {} as any;
+      }
+
+     
+      req.session.user = { id, name, role };
+
+      next(); 
+    } else {
+      return res.status(401).json({ error: 'Invalid token', details: false });
     }
- 
-    const authToken = token.replace(/bearer/gim, '').trim(); // Limpiar el token Bearer y obtener solo el token
-    const secret = process.env.SECRET_JWT;
-
-    if (!secret) {
-        return res.status(500).json({ message: 'JWT Not found',details:false });
-    }
-
-    try {
-        const decoded: any = jwt.verify(authToken, secret); 
-
-        if (decoded) {
-            const { id, name, role } = decoded; 
-
-            req.session.user = { id, name, role };
-
-            next(); 
-        } else {
-            return res.status(401).json({ error: 'Invalid token',details:false });
-        }
-    } catch (error) {
-        return res.status(500).json({ message: 'Internal server error al verificar el token',details:false });
-    }
+  } catch (error) {
+    return res.status(500).json({ message: 'Error verifying token', details: false });
+  }
 };
-
