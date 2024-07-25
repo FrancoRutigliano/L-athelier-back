db45dc485b91643aa5334654d4db9b65cb8108e7
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-07-25 15:17:19 -0300

    add middleware admin jwt

---
+++ src/shared/infrastructure/middlewares/authAdmin/midlewareAdminJWT.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { sessionEntity } from '../auth/entity/sessionEntity';
+
+export const verifySessionAdmin = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+  }
+
+  const token = authHeader.replace(/Bearer\s+/i, '').trim(); // Clean token and remove Bearer prefix
+  const secret = process.env.SECRET_JWT;
+
+  if (!secret) {
+    return res.status(500).json({ error: 'Internal server error: Secret key not defined' });
+  }
+  
+  try {
+    const decoded= jwt.verify(token, secret) as sessionEntity;
+    const role = decoded.role;
+
+    if(!role){
+        return res.status(401).json({ error: 'Unauthorized: dont have permissions ' });
+    }
+    next(); // Token is valid, proceed to the next middleware/route
+  } catch (error) {
+    return res.status(401).json({ error: 'Invalid or expired token' });
+  }
+};
