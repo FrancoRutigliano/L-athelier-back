9ec2e9c4e61d7ec8425f3de10e6fdf189c44f5d0
Franco Rutigliano <francorutigliano11@gmail.com>
2024-07-25 12:40:53 -0300

    modified

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import EmployeeRouter from "./Employee/infrastructure/routes/employeeRoutes";
 import SessionRouter from "./Session/infrastructure/routes/sessionRoutes";
-import session from "express-session";
-import { sessionEntity } from "./shared/infrastructure/middlewares/auth/entity/sessionEntity";
+//import session from "express-session";
+//import { sessionEntity } from "./shared/infrastructure/middlewares/auth/entity/sessionEntity";
+//import { MemoryStore } from 'express-session';
 
 
 
@@ -20,16 +21,20 @@ if (!secretJWT) {
   throw new Error("No hay clave secreta para JWT");
 }
 
-declare module "express-session" {
-  interface SessionData {
-    user?: sessionEntity; //defino como sera el atributo user de las req.session.user
-  }
-}
+// declare module "express-session" {
+//   interface SessionData {
+//     user?: sessionEntity; //defino como sera el atributo user de las req.session.user
+//   }
+// }
 
-app.use(session({  //configuro para que la app utilize las sessiones de express
-  secret: secretJWT,
-  resave: false,
-  saveUninitialized: true,
-}));
+
+// app.use(session({
+//   cookie: { maxAge: 86400000 },
+//   store: new MemoryStore({
+
+//   }),
+//   resave: false,
+//   secret: 'keyboard cat'
+// }))
 
 
