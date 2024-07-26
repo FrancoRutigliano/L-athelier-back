67d633f396c21af4efff95543ff22d1fbfa5c470
FrancoRutigliano <francorutigliano11@gmail.com>
2024-07-26 10:16:38 -0300

    adding cors config

---
+++ src/config/cors.ts
@@ -0,0 +1,13 @@
+import { CorsOptions } from 'cors';
+
+export const corsConfig: CorsOptions = {
+    origin: function(origin, callback) {
+        const whitelist = [process.env.FRONTEND_URL];
+
+        if(whitelist.includes(origin)) {
+            return callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}
